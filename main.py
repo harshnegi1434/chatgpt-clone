f7186c5c18f702bc98f7ad9a76e53b12cab76575
Harsh Negi <harshnegi2002@gmail.com>
2023-06-05 23:39:46 +0530

    MongoDB added

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
+import openai
+
+openai.api_key = "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"
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://harshnegi1434:dFV1pgC7mug6tWbv@gptclone.18xwqt4.mongodb.net/chatgpt"
+mongo = PyMongo(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html");
+    chats = mongo.db.chats.find({})
+    mychats = [chat for chat in chats]
+    print(mychats)
+    return render_template("index.html", mychats = mychats);
 
 @app.route("/api", methods={"GET", "POST"})
 def qa():
     if request.method == "POST":
-        data= {"result" : "Thank You"}
-        return jsonify(data)
+        print(request.json)
+        question = request.json.get("question")
+        chat = mongo.db.chats.find_one({"question":question})
+        if chat:
+            data = {"result" : f"{chat['answer']}"}
+            return jsonify(data)
+        else:
+            # response = openai.Completion.create(
+            #     model="text-davinci-003",
+            #     prompt=question,
+            #     temperature=1,
+            #     max_tokens=256,
+            #     top_p=1,
+            #     frequency_penalty=0,
+            #     presence_penalty=0
+            # )
+            # data= {{"question": question, "answer": response["choices"][0]["text"]}}
+            data= {"result" : f"Answer of {question}"}
+            mongo.db.chats.insert_one({"question": question, "answer": f"Answer from OPENAI for {question}"})
+            return jsonify(data)
     data = {"result" : "Thank You"}
     return jsonify(data)
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,24 @@
-sendButton.addEventListener("click", () => {
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      }, body: JSON.stringify(data),
+    });
+    return response.json(); 
+  }
+  
+const button = document.getElementById("sendButton")
+button.addEventListener("click", async() => {
     const questionInput = document.getElementById("questionInput").value;
     document.getElementById("questionInput").value = "";
     document.querySelector(".right2").style.display = "block";        
     document.querySelector(".right1").style.display = "none";
-        
-    question1.InnerHTML = questionInput;
-    question2.InnerHTML = questionInput;
-});
 
+    question1.innerHTML = questionInput
+    question2.innerHTML = questionInput
+
+    //Get the Answer
+    let result = await postData("/api", {"question" : questionInput})
+    answer.innerHTML = result.result
+});
