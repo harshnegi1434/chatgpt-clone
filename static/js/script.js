bce6da6c3c0250f78b74ec63eb4cf091a0885de9
Harsh Negi <harshnegi2002@gmail.com>
2023-06-10 21:27:29 +0530

    Update Features

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,27 @@ button.addEventListener("click", async() => {
     answer.innerHTML = result.answer
     //answer.innerHTML = result.result
 });
+
+const newbutton = document.getElementById("newButton")
+newbutton.addEventListener("click", async() => {
+  document.querySelector(".right1").style.display = "flex";
+  document.querySelector(".right1").style.display = "none";
+  location.reload();
+})
+
+const rebutton = document.getElementById("reButton")
+rebutton.addEventListener("click", async() => {
+    const questionInput = document.getElementById("newquestionInput").value;
+    document.getElementById("questionInput").value = "";
+    document.querySelector(".right2").style.display = "block";        
+    document.querySelector(".right1").style.display = "none";
+
+    question1.innerHTML = questionInput
+    question2.innerHTML = questionInput
+
+    answer.innerHTML = "Loading..."    
+    //Get the Answer
+    let result = await postData("/api", {"question" : questionInput})
+    answer.innerHTML = result.answer
+    //answer.innerHTML = result.result
+});
