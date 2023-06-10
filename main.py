84e58dcd9a372b994bbc32815cf9b9e5b4522a67
Harsh Negi <harshnegi2002@gmail.com>
2023-06-10 20:54:45 +0530

    Updated Code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from bardapi import Bard
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://harshnegi1434:dFV1pgC7mug6tWbv@gptclone.18xwqt4.mongodb.net/chatgpt"
+app.config["MONGO_URI"] = "mongodb+srv://harshnegi1434:6jtJRfySD5xrnMt4@projects.0rrxqmw.mongodb.net/GPTClone"
 mongo = PyMongo(app)
 
 token = 'XAiB_aiN9bJ_U4pGJTWcwwTlPnhJLt6A_enGufQV9o0q0H13Z6YRuMlRZCP1nN_HW9_ZaA.'
@@ -13,13 +13,13 @@ bard = Bard(token=token)
 def home():
     chats = mongo.db.chats.find({})
     mychats = [chat for chat in chats]
-    print(mychats)
+    #print(mychats)
     return render_template("index.html", mychats = mychats);
 
 @app.route("/api", methods={"GET", "POST"})
 def qa():
     if request.method == "POST":
-        print(request.json)
+        #print(request.json)
         question = request.json.get("question")
         chat = mongo.db.chats.find_one({"question":question})
         if chat:
@@ -27,7 +27,7 @@ def qa():
             return jsonify(data)
         else:
             answer = bard.get_answer(question)['content']
-            print (answer)
+            #print (answer)
             data= {"question": question, "answer": answer}
             mongo.db.chats.insert_one({"question": question, "answer": answer})
             return jsonify(data)
